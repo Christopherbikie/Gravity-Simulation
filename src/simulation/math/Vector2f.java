052377f3ce8eb3e71dce7723d4050a795eb26e51
Christopher Hall <christopher.hall.3609@gmail.com>
2016-04-19 21:36:01 +1000

    Finished Vector2f

--- src/simulation/math/Vector2f.java
+++ src/simulation/math/Vector2f.java
@@ -6,25 +6,61 @@ package simulation.math;
 public class Vector2f {
 	private float x, y;
 
+	/**
+	 * Creates an instance of Vector2f with 0 magnitude.
+	 */
 	public Vector2f() {
 		this.x = 0;
 		this.y = 0;
 	}
 
+	/**
+	 * Creates an instance of Vector2f with specified x and y components.
+	 *
+	 * @param x X component
+	 * @param y Y component
+	 */
 	public Vector2f(float x, float y) {
 		this.x = x;
 		this.y = y;
 	}
 
+	/**
+	 * Returns the unit vector of the current instance.
+	 *
+	 * @return the unit vector of the current instance
+	 */
 	public Vector2f getUnitVector() {
 		return new Vector2f(x / getLength(), y / getLength());
 	}
 
+	/**
+	 * Adds two vectors and returns the result.
+	 *
+	 * @param vector The vector to add to the current instance
+	 * @return The result of the operation
+	 */
 	public Vector2f add(Vector2f vector) {
 		return new Vector2f(x + vector.getX(), y + vector.getY());
 	}
 
-	public Vector2f dot(float scalar) {
+	/**
+	 * Subtracts two vectors and returns the result.
+	 *
+	 * @param vector The vector to subtract from the current instance
+	 * @return The result of the operation
+	 */
+	public Vector2f sub(Vector2f vector) {
+		return new Vector2f(x - vector.getX(), y - vector.getY());
+	}
+
+	/**
+	 * Multiplies (or scales) the current instance by a given value.
+	 *
+	 * @param scalar The amount to scale by
+	 * @return The result of the operation
+	 */
+	public Vector2f mul(float scalar) {
 		Vector2f result = new Vector2f();
 		result.setX(x * scalar);
 		result.setY(y * scalar);
@@ -52,7 +88,34 @@ public class Vector2f {
 		this.y = vector.getY();
 	}
 
+	/**
+	 * Calculates the length of the current vector.
+	 *
+	 * @return The result of the operation
+	 */
 	public float getLength() {
 		return (float) Math.sqrt(x * x + y * y);
+	}
+
+	/**
+	 * For debugging. Returns a String representation of the vector.
+	 *
+	 * @return String representation of the vector
+	 */
+	@Override
+	public String toString() {
+		StringBuilder result = new StringBuilder();
+		Vector2f unitVector = this.getUnitVector();
+
+		result.append(this.getClass().getName()).append(" Object {\n");
+		result.append("    X component: ").append(x).append("\n");
+		result.append("    Y component: ").append(y).append("\n");
+		result.append("    Length: ").append(getLength()).append("\n");
+		result.append("    Unit Vector {\n");
+		result.append("        X component: ").append(unitVector.getX()).append("\n");
+		result.append("        Y component: ").append(unitVector.getY()).append("\n");
+		result.append("        Length: ").append(unitVector.getLength()).append("\n    }\n}");
+
+		return result.toString();
 	}
 }
