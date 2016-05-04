5f3cc744b62aeecd90777c960c095c857e812ec5
Christopher Hall <christopher.hall.3609@gmail.com>
2016-05-04 19:17:34 +1000

    Simulation speed is displayed in window title

--- src/maths/Physics.java
+++ src/maths/Physics.java
@@ -6,9 +6,10 @@ package maths;
 public class Physics {
 
 	public static final double G = 6.67408e-11;
+	public static final double METERS_PER_AU = 1.496e+11;
 
 	public static double getForce(double mass1, double mass2, double distance) {
-		return G * mass1 * mass2 / (distance * distance);
+		return G * mass1 * mass2 / (distance * METERS_PER_AU * distance * METERS_PER_AU);
 	}
 
 	public static double getAcceleration(double force, double mass) {
