95873706b9a092916241ca3efa5f0e623dc006ba
Christopher Hall <christopher.hall.3609@gmail.com>
2016-05-10 20:50:53 +1000

    Camera is controlled by the mouse

--- src/entities/Camera.java
+++ src/entities/Camera.java
@@ -1,7 +1,9 @@
 package entities;
 
 import org.lwjgl.input.Keyboard;
+import org.lwjgl.input.Mouse;
 import org.lwjgl.util.vector.Vector3f;
+import util.Clock;
 
 /**
  * Created by Christopher on 24/04/2016.
@@ -19,7 +21,7 @@ public class Camera {
 	/**
 	 * Float for the camera's speed
 	 */
-	private float speed = 0.1f;
+	private float speed = 1f;
 
 	/**
 	 * Constructor for the camera.
@@ -40,27 +42,36 @@ public class Camera {
 	 * Move the camera according to user keyboard input
 	 */
 	public void move() {
+		float delta = Clock.Delta();
 		if (Keyboard.isKeyDown(Keyboard.KEY_W)) {
-			position.x -= (float) (Math.sin(-yaw * Math.PI / 180) * speed);
-			position.z -= (float) (Math.cos(-yaw * Math.PI / 180) * speed);
+			position.x -= (float) (Math.sin(-yaw * Math.PI / 180) * speed * delta);
+			position.z -= (float) (Math.cos(-yaw * Math.PI / 180) * speed * delta);
 		}
 		if (Keyboard.isKeyDown(Keyboard.KEY_S)) {
-			position.x += (float) (Math.sin(-yaw * Math.PI / 180) * speed);
-			position.z += (float) (Math.cos(-yaw * Math.PI / 180) * speed);
+			position.x += (float) (Math.sin(-yaw * Math.PI / 180) * speed * delta);
+			position.z += (float) (Math.cos(-yaw * Math.PI / 180) * speed * delta);
 		}
 		if (Keyboard.isKeyDown(Keyboard.KEY_A)) {
-			position.x += (float) (Math.sin((-yaw - 90) * Math.PI / 180) * speed);
-			position.z += (float) (Math.cos((-yaw - 90) * Math.PI / 180) * speed);
+			position.x += (float) (Math.sin((-yaw - 90) * Math.PI / 180) * speed * delta);
+			position.z += (float) (Math.cos((-yaw - 90) * Math.PI / 180) * speed * delta);
 		}
 		if (Keyboard.isKeyDown(Keyboard.KEY_D)) {
-			position.x += (float) (Math.sin((-yaw + 90) * Math.PI / 180) * speed);
-			position.z += (float) (Math.cos((-yaw + 90) * Math.PI / 180) * speed);
+			position.x += (float) (Math.sin((-yaw + 90) * Math.PI / 180) * speed * delta);
+			position.z += (float) (Math.cos((-yaw + 90) * Math.PI / 180) * speed * delta);
 		}
 		if (Keyboard.isKeyDown(Keyboard.KEY_SPACE))
-			position.y += speed;
+			position.y += speed * delta;
 		if (Keyboard.isKeyDown(Keyboard.KEY_LSHIFT))
-			position.y -= speed;
+			position.y -= speed * delta;
 
+		if (Mouse.isButtonDown(0)) {
+			float angleChange = Mouse.getDX() * 0.3f;
+			yaw += angleChange;
+		}
+		if (Mouse.isButtonDown(0)) {
+			float angleChange = Mouse.getDY() * 0.1f;
+			pitch -= angleChange;
+		}
 		if (Keyboard.isKeyDown(Keyboard.KEY_UP))
 			pitch -= 1f;
 		if (Keyboard.isKeyDown(Keyboard.KEY_DOWN))
